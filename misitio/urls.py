b063e9db858998b85d9f78c340ea9dfca5558470
Jose Francisco Guerrero <jsfrncscg@gmail.com>
2013-06-16 21:27:10 -0430

    Vaciando el repositorio

--- misitio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-admin.autodiscover()
-
-urlpatterns = patterns('',
-    url(r'^encuestas/', include('encuestas.urls', namespace="encuestas")),
-    url(r'^admin/', include(admin.site.urls)),
-)
