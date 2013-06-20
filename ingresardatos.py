59d29d2ee92e98baf4a29e8b0a82815cf477ac3c
Jose Francisco Guerrero <jsfrncscg@gmail.com>
2013-06-19 22:50:14 -0430

    - Corregidos errores en el recorrido del archivo Packages. - Ahora se utiliza el metodo "manual" para recorrer el archivo Packages.

--- ingresardatos.py
+++ ingresardatos.py
@@ -13,12 +13,19 @@ class dependencia_simple(object):
         self.nombre = nombre
         self.version = version
 
-def buscar(paquete, nombre_archivo):
-    #nombre_archivo.jump(0)
-    for section in nombre_archivo:
+def buscar(paquete, archivo):
+    parar = True
+    archivo.jump(0)
+    while parar != False:
+        parar = archivo.step()
+        if archivo.section.get('Package') == paquete:
+            return archivo.section
+        
+def buscar_for(paquete, archivo):
+    for section in archivo:
         if section.get('Package') == paquete:
             return section
-    
+        
 def buscar_mantenedor(seccion):
     nombreMan, correoMan = email.Utils.parseaddr(seccion.get("Maintainer"))
     mantenedorActual = Mantenedor.objects.filter(nombre_completo = nombreMan, correo = correoMan)
@@ -31,15 +38,16 @@ def buscar_mantenedor(seccion):
 
 def buscar_paquete(seccion):
     if seccion == None:
-        print "Paquete no encontrado"
+        #print seccion
+        #print "Paquete no encontrado"
         return
     nombre_paquete = seccion.get("Package")
     mantenedor_paquete = buscar_mantenedor(seccion)
     arqui_paquete = seccion.get("Architecture")
     paquete_existe = Paquete.objects.filter(nombre = nombre_paquete, 
-                                               mantenedor = mantenedor_paquete,
-                                               arquitectura = arqui_paquete
-                                               )
+                                            mantenedor = mantenedor_paquete,
+                                            arquitectura = arqui_paquete
+                                            )
     if len(paquete_existe):
         return paquete_existe[0]
     else:
@@ -88,6 +96,9 @@ def listar_dependencias(seccion):
     
 def buscar_dep_simple(seccion):
     paquete = buscar_paquete(seccion)
+    # Solucion temporal mientras identifico el problema
+    if paquete == None:
+        return
     dep_existe = DependenciaSimple.objects.filter(dep = paquete)
     if len(dep_existe) > 1:
         return dep_existe[0]
@@ -104,38 +115,35 @@ def registrar_dependencias_for(paquete, lista_dependencias, archivo):
         paquete.dependenciaSimple.add(ds)
         
 def registrar_lista_paquetes_for(archivo):
-     for seccion in archivo:
-         paquete = buscar_paquete(seccion)
-         print paquete
-         simple = listar_dependencias(seccion)
-         registrar_dependencias(paquete, simple, archivo)
-         
+    for seccion in archivo:
+        paquete = buscar_paquete(seccion)
+        simple = listar_dependencias(seccion)
+        registrar_dependencias(paquete, simple, archivo)
+        
 def registrar_dependencias(paquete, lista_dependencias, archivo):
     for dep in lista_dependencias:
         d = dep.nombre
         sect = buscar(d, archivo)
-        ds = buscar_dep_simple(sect)
-        paquete.dependenciaSimple.add(ds)
-        
-def registrar_lista_paquetes(archivo):
-     for seccion in archivo:
-         paquete = buscar_paquete(seccion)
-         print paquete
-         simple = listar_dependencias(seccion)
-         registrar_dependencias(paquete, simple, archivo)
-         
-        
+        if sect != None:
+            ds = buscar_dep_simple(sect)
+            paquete.dependenciaSimple.add(ds)
+        else:
+            print "Probablemente", d, "sea un paquete virtual provisto por otro paquete, de momento no se registrara"
+                
 a1 = apt_pkg.TagFile(open('/home/fran/Packages'))
-a2 = apt_pkg.TagFile(open('/home/fran/Packages2'))
 
+#print buscar("gnustep-fslayout-fhs", a1)
+
+tmpoffset = 0
 parar = True
-
 while parar != False:
+    a1.jump(tmpoffset)
     parar = a1.step()
     seccion_actual = a1.section
+    tmpoffset = a1.offset()
+    #print seccion_actual
     paquete = buscar_paquete(seccion_actual)
     print paquete
     simple = listar_dependencias(seccion_actual)
     registrar_dependencias(paquete, simple, a1)
-
-#registrar_lista_paquetes(a1)
+    
